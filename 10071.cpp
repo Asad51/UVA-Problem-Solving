d4090319900dfd7da8357c4e6fe1b58ea5f9ee58
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2018-04-27 08:36:27 +0600

    Solution for 10071

---
+++ 10071.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int v,t,d;
+    while(cin>>v>>t)
+    {
+        d=2*v*t;
+        cout<<d<<endl;
+    }
+    return 0;
+}
