e4ed0125f8d8daed478af72d2156f45efbc39e68
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2018-09-13 21:34:35 +0600

    modified

--- uva-11503.cpp
+++ uva-11503.cpp
@@ -3,20 +3,33 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	freopen("in.txt", "r", stdin); 
-	freopen("out.txt", "w", stdout);
+	//freopen("in.txt", "r", stdin); 
+	//freopen("out.txt", "w", stdout);
 	int t;
 	scanf("%d", &t);
 	while(t--){
 		int n;
 		scanf("%d", &n);
-		set<string> persons;
+		//set<string> persons;
+		map<string, set<string> >persons;
 		string p1, p2;
 		while(n--){
+			set<string>tmp;
+			set<string>::iterator it;
 			cin>>p1>>p2;
-			persons.insert(p1);
-			persons.insert(p2);
-			cout<<persons.size()<<"\n";
+			persons[p1].insert(p2);
+			persons[p2].insert(p1);
+			for(it = persons[p1].begin(); it!=persons[p1].end(); ++it){
+				tmp.insert(*it);
+				persons[p2].insert(*it);
+			}
+			for(it = persons[p2].begin(); it!=persons[p2].end(); ++it){
+				tmp.insert(*it);
+				persons[p1].insert(*it);
+			}
+			cout<<persons[p1].size()<<" "<<persons[p2].size()<<endl;
+			cout<<tmp.size()<<"\n";
+			tmp.clear();
 		}
 		persons.clear();
 	}
