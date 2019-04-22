b02858c8e223e24bc7ec6e13ab457ed229737b16
Md. Asadul Islam <asad.cse.ru.15@gmail.com>
2019-04-22 21:36:33 +0600

    c version

--- 10130.cpp
+++ 10130.cpp
@@ -1,54 +1,56 @@
-#include "bits/stdc++.h"
-using namespace std;
+#include <stdio.h>
 
-class knapsack{
-public:
-    int max(int a, int b){
-		    return (a>b)? a: b;
-	  }
-	  int findMaxValue(int n, int w, int p[], int wt[]){
-		    int i, j;
-		    int x[n+1][w+1] = {{0}};
-		    for(i = 0; i<=n; i++){
-			      for(j = 0; j<=w; j++){
-				        if(i==0 || j==0)
-					          x[i][j] = 0;
-				        else if(wt[i-1]<=j)
-					         x[i][j] = max(p[i-1] + x[i-1][j-wt[i-1]], x[i-1][j]);
-				        else
-					         x[i][j] = x[i-1][j];
-			      }
-		    }
-		    return x[n][w];
-	  }
-};
-
-int main(int argc, char const *argv[])
+int max(int a, int b)
 {
-	  ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+	if (a > b)
+		return a;
+	return b;
+}
 
-    knapsack sack;
-  	int T;
-  	cin>>T;
-  	while(T--){
-  		  int N;
-  		  cin>>N;
-  		  int P[N], W[N];
-  		  for(int i=0; i<N; ++i){
-  			    cin>>P[i]>>W[i];
-  		  }
+int knap_sack(int p[], int w[], int n, int c)
+{
+	int kn[n + 1][c + 1];
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <= c; j++)
+		{
+			if (!i || !j)
+				kn[i][j] = 0;
+			else if (w[i - 1] <= j)
+			{
+				kn[i][j] = max(p[i - 1] + kn[i - 1][j - w[i - 1]], kn[i - 1][j]);
+			}
+			else
+			{
+				kn[i][j] = kn[i - 1][j];
+			}
+		}
+	}
+	return kn[n][c];
+}
 
-  		  int G;
-  		  cin>>G;
-  		  int w;
-  		  int total = 0;
-  		  for(int i=0; i<G; i++){
-  			    cin>>w;
-  			    total += sack.findMaxValue(N, w, P, W);
-  		  }
-  		  cout<<total<<"\n";
-  	}
-	  return 0;
+int main()
+{
+	int t, n, g;
+	scanf("%d", &t);
+	while (t--)
+	{
+		scanf("%d", &n);
+		int p[1001], w[1001];
+		for (int i = 0; i < n; i++)
+		{
+			scanf("%d %d", &p[i], &w[i]);
+		}
+
+		scanf("%d", &g);
+		int tp = 0, c;
+		for (int i = 0; i < g; i++)
+		{
+			scanf("%d", &c);
+			tp += knap_sack(p, w, n, c);
+		}
+		printf("%d\n", tp);
+	}
+
+	return 0;
 }
